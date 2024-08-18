bd38e1f286c392cc003f5eb65e2904c1831522c7
โมกข์ มาอาจ <owl@Macintosh.local>
2024-08-18 15:42:40 +0700

    button and binding function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,22 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-tutorial';
+  imgURL =
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.leagueoflegends.com%2Fth-th%2Fchampions%2Fteemo%2F&psig=AOvVaw28eC0DsworNUWbPx8DM5Nn&ust=1724056517854000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIjT95OR_ocDFQAAAAAdAAAAABAE';
+
+  number = 1;
+
+  changeNumber( action : '+' | '-' ) {
+    if ( action === '+' ) {
+      this.number += 1;
+    }
+
+    if ( action === '-' ) {
+      this.number -= 1;
+    }
+  }
 }
