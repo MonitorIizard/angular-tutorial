a78536084dba7fa4566f0b9f1430312c702ba566
Mok <mokmaard646@gmail.com>
2024-08-18 17:23:55 +0700

    lecture : route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { MembersComponent } from './page/members/members.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {
+    path : 'members',
+    component : MembersComponent
+  }
+];
