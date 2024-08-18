9b6a5f62cadab15904290cdfe1e598dd5bfa6727
Mok <mokmaard646@gmail.com>
2024-08-18 16:31:48 +0700

    lecture: @Input

--- src/app/shared/img/img.component.ts
+++ src/app/shared/img/img.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   // standalone: true,
   // imports: [], if we want to use shared module remove import and standalone
   templateUrl: './img.component.html',
-  styleUrl: './img.component.css'
+  styleUrl: './img.component.css',
 })
 export class ImgComponent {
-
+  @Input() imgURL =
+    'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg';
 }
